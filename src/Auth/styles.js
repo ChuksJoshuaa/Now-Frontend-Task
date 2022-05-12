a9437edb7f47a3b8515e7dbd3812ac87c5ca0bbc
D-coder <chuksjoshuaa@gmail.com>
2022-05-12 08:19:45 +0100

    Update styles.js

--- src/Auth/styles.js
+++ src/Auth/styles.js
@@ -29,11 +29,37 @@ export default makeStyles((theme) => ({
     fontSize: "1em",
     color: "#b7bfc9",
     marginLeft: "0.5em",
+    [theme.breakpoints.down("285")]: {
+      fontSize: "0.9em",
+    },
   },
   wordyy: {
     fontFamily: `"Rajdhani", sans-serif`,
     color: "rgba(15, 15, 15, 0.9)",
     fontSize: "1em",
+  },
+  wording: {
+    marginLeft: "12em",
+
+    [theme.breakpoints.down("432")]: {
+      marginLeft: "10em",
+    },
+    [theme.breakpoints.down("405")]: {
+      marginLeft: "8em",
+    },
+
+    [theme.breakpoints.down("361")]: {
+      marginLeft: "6em",
+    },
+
+    [theme.breakpoints.down("318")]: {
+      marginLeft: "3em",
+    },
+
+    [theme.breakpoints.down("285")]: {
+      fontSize: "0.9em",
+      marginLeft: "5em",
+    },
   },
   wordy: {
     fontFamily: `"Rajdhani", sans-serif`,
@@ -41,5 +67,8 @@ export default makeStyles((theme) => ({
     fontSize: "1em",
     marginRight: "1em",
     textTransform: "none",
+    [theme.breakpoints.down("285")]: {
+      fontSize: "0.9em",
+    },
   },
 }));
